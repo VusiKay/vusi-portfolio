0fdd33d52fbbbb6d63f73f17490e3225b9e6db91
Vusi Mthombeni <voltairmthombeni@gmail.com>
2025-11-04 20:48:49 +0200

    feat: update App.jsx with latest changes

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,6 +1,11 @@
-import Header from './Header'
-import Footer from './Footer'
-import Body from './Body'
+import { Routes, Route } from 'react-router-dom'
+
+import Footer from './Components/Layout/Footer'
+import Body from './routes/Home'
+/* import Contact from './routes/Contact' */
+
+import MainNavigation from './Components/Layout/MainNavigation'
+import About from './routes/About'
 
 function App() {
   
@@ -8,11 +13,20 @@ function App() {
   return (
     
     <div>
+<Home />
+   {/* <Header />
+    <Body /> */}
+    {/* <Contact /> */}
+ {/*    <About />
+    <Footer /> */}
+ {/* <MainNavigation /> */}
+{/* <Routes>
+ 
+<Route path='/' element={<AllMeetupsPage />}/>
+<Route path='/new-meetup' element={<NewMeetupPage />}/>
+<Route path='/favorites' element={<FavoritesPage />}/>
 
-    <Header />
-    <Body />
-    <Footer />
-
+</Routes> */}
     </div>
   )
 }
